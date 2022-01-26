15e15030e9260fa086715a6c9c66f23c4698f487
曦暮流年 <98019029+ximuliunian@users.noreply.github.com>
2022-01-26 15:25:21 +0800

    Delete youxi directory

--- youxi/太空射手/shooter-html5/src/js/powerup.js
+++
@@ -1,9 +0,0 @@
-define(['jquery', 'util', 'entity'], function($, util, entity) {
-  var module = util.extend(entity.entity);
-
-  module.prototype.setup = function(data) {
-    $(this.el).addClass('power-up').text(data.k);
-  };
-
-  return module;
-});
